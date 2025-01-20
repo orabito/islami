483598aafc6a5147abc3035e827ca0b37dcf005d
Ibrahim Oraby <orabi224488@gmail.com>
2025-01-20 18:23:12 +0200

    +add  FloatingActionButton

--- lib/ui/home/tabs/sebha_tab.dart
+++ lib/ui/home/tabs/sebha_tab.dart
@@ -17,94 +17,103 @@ class _SebhaTabState extends State<SebhaTab> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage(AssetsManager.sebhaBack),
-        fit: BoxFit.fill,
-      )),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
-        child: Column(
-          children: [
-            Expanded(
-              flex: 3,
-              child: Image.asset(
-                AssetsManager.lago,
-                fit: BoxFit.cover,
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            const Expanded(
-              flex: 2,
-              child: Text(
-                "سَبِّحِ اسْمَ رَبِّكَ الأعلى",
-                style: TextStyle(
-                    fontSize: 30,
-                    fontFamily: "janna",
-                    fontWeight: FontWeight.w700,
-                    color: ColorsManager.tertiary),
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Stack(
-              alignment: Alignment.topCenter,
+    return SafeArea(
+      child: Scaffold(
+        
+        floatingActionButton: FloatingActionButton(onPressed: (){
+          
+        }),
+        body: Container(
+          
+          width: double.infinity,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage(AssetsManager.sebhaBack),
+            fit: BoxFit.fill,
+          )),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+            child: Column(
               children: [
-                Image.asset(
-                  AssetsManager.sebhaTop,
-                  height: MediaQuery.of(context).size.height * .1,
-                  fit: BoxFit.cover,
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 60),
-                  child: InkWell(
-                    onTap: () {
-                      azkarSabahCounter();
-                    },
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Transform.rotate(
-                          angle: turn,
-                          transformHitTests: true,
-                          child: SvgPicture.asset(
-                            AssetsManager.sebhaNumber,
-                            fit: BoxFit.cover,
-                            width: MediaQuery.of(context).size.width * .9,
-                          ),
-                        ),
-                        Text(
-                          azkarSabah[index],
-                          style: const TextStyle(
-                              fontSize: 36,
-                              fontFamily: "janna",
-                              fontWeight: FontWeight.bold,
-                              color: ColorsManager.tertiary),
-                        ),
-                        Positioned(
-                          bottom: 75,
-                          child: Text(
-                            "$count",
-                            style: const TextStyle(
-                                fontSize: 36,
-                                fontFamily: "janna",
-                                fontWeight: FontWeight.bold,
-                                color: ColorsManager.tertiary),
-                            textAlign: TextAlign.end,
-                          ),
-                        ),
-                      ],
-                    ),
+                Expanded(
+                  flex: 3,
+                  child: Image.asset(
+                    AssetsManager.lago,
+                    fit: BoxFit.cover,
                   ),
                 ),
+                const SizedBox(
+                  height: 8,
+                ),
+                const Expanded(
+                  flex: 2,
+                  child: Text(
+                    "سَبِّحِ اسْمَ رَبِّكَ الأعلى",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: "janna",
+                        fontWeight: FontWeight.w700,
+                        color: ColorsManager.tertiary),
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Image.asset(
+                      AssetsManager.sebhaTop,
+                      height: MediaQuery.of(context).size.height * .1,
+                      fit: BoxFit.cover,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 60),
+                      child: InkWell(
+                        onTap: () {
+                          azkarSabahCounter();
+                        },
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Transform.rotate(
+                              angle: turn,
+                              transformHitTests: true,
+                              child: SvgPicture.asset(
+                                AssetsManager.sebhaNumber,
+                                fit: BoxFit.cover,
+                                width: MediaQuery.of(context).size.width * .9,
+                              ),
+                            ),
+                            Text(
+                              azkarSabah[index],
+                              style: const TextStyle(
+                                  fontSize: 36,
+                                  fontFamily: "janna",
+                                  fontWeight: FontWeight.bold,
+                                  color: ColorsManager.tertiary),
+                            ),
+                            Positioned(
+                              bottom: 75,
+                              child: Text(
+                                "$count",
+                                style: const TextStyle(
+                                    fontSize: 36,
+                                    fontFamily: "janna",
+                                    fontWeight: FontWeight.bold,
+                                    color: ColorsManager.tertiary),
+                                textAlign: TextAlign.end,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
